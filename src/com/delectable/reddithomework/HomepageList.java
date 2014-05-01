db4d76a4bc099592a73b047c5130cb5adda0d304
Wayne <houngchiu@gmail.com>
2014-05-01 18:25:51 -0400

    cleaned up code a bit

--- src/com/delectable/reddithomework/HomepageList.java
+++ src/com/delectable/reddithomework/HomepageList.java
@@ -33,29 +33,36 @@ public class HomepageList extends Fragment{
 	MyListAdapter mListAdapter; 
 	List<Child> mChildren = new ArrayList<Child>();
 
+	//initing our rest endpoint class
 	RestAdapter restAdapter = new RestAdapter.Builder()
-	  .setEndpoint("http://www.reddit.com") // The base API endpoint.
-	  .build();
+	.setEndpoint("http://www.reddit.com") // The base API endpoint.
+	.build();
 
-	RedditEndpoints redditService = restAdapter.create(RedditEndpoints.class);
-	
+	RedditEndpoints redditEndpoints = restAdapter.create(RedditEndpoints.class);
+
 	/**We use this hold on to the after ID that we use to get to the next page*/
 	private String mAfter;
-	/**Used to keep track of when a rest request is taking place, so we don't end up making double calls*/
-	private boolean isLoading;
 	/**The progressBar is shown when the user scrolls to the bottom of the list and the GET request for the next page of data is underway.*/
 	private ProgressBar mProgressBar; 
 	/**This shows up in our header when we've exhausted the data source.*/
 	private TextView mEndTextView; 
-	
+	/**
+	 * If we are showing the ProgressBar, then we must be attempting a GET request currently. 
+	 * Used to make sure we aren't making double calls
+	 */
+	private boolean isLoading()
+	{
+		return mProgressBar.getVisibility()==View.VISIBLE; 
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
 	{
 		View rootView = inflater.inflate(R.layout.fragment_main, container,	false);
-		
+
 		//listview setup
 		mListView = (ListView)rootView.findViewById(R.id.listview);
-		
+
 		//footer setup
 		View footer = inflater.inflate(R.layout.footer, mListView, false);
 		mProgressBar = (ProgressBar)footer.findViewById(R.id.progressBar);
@@ -66,83 +73,81 @@ public class HomepageList extends Fragment{
 		mListView.setOnItemClickListener(mItemClickListener);
 		mListView.setAdapter(mListAdapter = new MyListAdapter(mChildren));
 		mListView.setOnScrollListener(mScrollListener);
-		
+
 		//setting up button to invoke call
 		rootView.findViewById(R.id.button).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				redditService.getHomepage(null, callback);
-				isLoading = true;
+				redditEndpoints.getHomepage(null, callback);
 				mProgressBar.setVisibility(View.VISIBLE);
 			}
 		});
 		return rootView;
 	}
-	
+
 	private Callback<Page> callback = new Callback<Page>() {
 		@Override
 		public void success(Page page, Response arg1) {
-			
-			//done retrieving items, so we can set this back to false
-			isLoading = false;
+
+			//done retrieving items, so we hide the progressBar
 			mProgressBar.setVisibility(View.GONE);
 
 			//grabbing hold of after so we can use it later when user scrolls to bottom of list
 			mAfter = page.getData().getAfter(); 
-			
+
 			//means we've reached the last page of the data source, and there is no page afterwards
 			if(mAfter==null) {
 				mEndTextView.setVisibility(View.VISIBLE);
 			}
-			
+
 			//adding the new onto array and refreshing the listview to show additions
 			List<Child> children = page.getData().getChildren(); 
 			mChildren.addAll(children); 
 			mListAdapter.notifyDataSetChanged(); 
 		}
 
-	    @Override
-	    public void failure(RetrofitError retrofitError) {
-	    	
-	    	//TODO need to handle error
-	    	isLoading = false;
+		@Override
+		public void failure(RetrofitError retrofitError) {
+
+			//TODO need to handle error
 			mProgressBar.setVisibility(View.GONE);
 
-	    }
+		}
 	};
-	
+
 	private OnScrollListener mScrollListener = new OnScrollListener() {
 		@Override
 		public void onScrollStateChanged(AbsListView view, int scrollState) {
 			//do nothing
 		}
-		
+
 		@Override
 		public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) 
 		{
 			//in the middle of a request, disallow a new REST request from happening
-			if(isLoading) {
+			if(isLoading()) {
 				return;
 			}
-			
+
 			//means we've reached the end of the list, so we disallow them from making another rest request
 			if(mEndTextView.getVisibility()==View.VISIBLE) {
 				return;
 			}
-			
-			//listen for when the user has reached the bottom of the list
-			 boolean loadMore = firstVisibleItem + visibleItemCount >= totalItemCount;
 
-			 //if they've reached the end of the list, then we init the GET call for the next page
-			 if(loadMore) {
-					redditService.getHomepage(mAfter, callback);
-					isLoading = true;
-					mProgressBar.setVisibility(View.VISIBLE);
-			 }
+			//listen for when the user has reached the bottom of the list
+			boolean loadMore = firstVisibleItem + visibleItemCount >= totalItemCount;
+
+			//if they've reached the end of the list, then we init the GET call for the next page
+			if(loadMore) {
+				redditEndpoints.getHomepage(mAfter, callback);
+				mProgressBar.setVisibility(View.VISIBLE);
+			}
 		}
 	};
-	
-	
+
+
+
+
 	OnItemClickListener mItemClickListener = new OnItemClickListener() {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
@@ -152,7 +157,7 @@ public class HomepageList extends Fragment{
 			view.setSelected(!selected);
 		}
 	};
-	
+
 
 
 
