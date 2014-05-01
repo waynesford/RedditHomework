478c2352fb214d76c88c10daa5ae51d1675e68fb
Wayne <houngchiu@gmail.com>
2014-05-01 08:51:37 -0400

    abstracted MyListAdapter into it's own class

--- src/com/delectable/reddithomework/HomepageList.java
+++ src/com/delectable/reddithomework/HomepageList.java
@@ -8,20 +8,16 @@ import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import com.delectable.model.Child;
-import com.delectable.model.Data2;
 import com.delectable.model.Page;
 import com.example.reddithomework.R;
 
@@ -92,74 +88,7 @@ public class HomepageList extends Fragment{
 		}
 	};
 	
-	private class MyListAdapter extends BaseAdapter
-	{
-		List<Child> mChildren; 
-		public MyListAdapter(List<Child> children)
-		{
-			mChildren = children; 
-		}
 
-		@Override
-		public int getCount() {
-			return mChildren.size();
-		}
 
-		@Override
-		public Child getItem(int position) {
-			return mChildren.get(position);
-		}
-
-		@Override
-		public long getItemId(int position) 
-		{
-			return position;
-		}
-		
-		@Override
-		public int getItemViewType(int position) 
-		{
-			Data2 data = getItem(position).getData();
-			if(data.getThumbnail()!=null && !data.getThumbnail().equals("")) {
-				return 0;
-			}
-			return 1; 
-		}
-		
-		@Override
-		public int getViewTypeCount() 
-		{
-			return 2;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) 
-		{
-			Data2 data = getItem(position).getData();
-			ImageRow view = (ImageRow)convertView; 
-			
-			//init row views
-			if(view==null) {
-				int height = getPx(parent.getContext(), 80);
-				if(data.getThumbnail()!=null && !data.getThumbnail().equals("")) {
-					view = new ImageRow(getActivity());
-				} else {
-					view = new TitleOnlyRow(getActivity()); 
-				}
-				//view.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, height));
-			}
-			//set data upon rows as they get shown on screen
-			view.setData(data);
-			return view;
-		}
-		
-		
-	}
-	
-	/**Converts dp to px*/
-	private int getPx(Context c, float dp) 
-	{
-		return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, c.getResources().getDisplayMetrics());
-	}
 
 }
