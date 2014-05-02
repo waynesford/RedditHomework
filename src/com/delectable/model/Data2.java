b2ea677ff52e1616b1452530cb707b27658db019
Wayne <houngchiu@gmail.com>
2014-05-02 14:41:22 -0400

    made Data2 parcelable

--- src/com/delectable/model/Data2.java
+++ src/com/delectable/model/Data2.java
@@ -1,15 +1,18 @@
 package com.delectable.model;
 
-public class Data2 {
-	
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Data2 implements Parcelable{
+
 	private String title;
 	private int score;
 	private int num_comments;
 	private String thumbnail;
-	
-    private boolean is_self;
-    private String selftext;
-    private String url;
+
+	private boolean is_self;
+	private String selftext;
+	private String url;
 
 	public String getTitle() {
 		return title;
@@ -35,8 +38,8 @@ public class Data2 {
 	public void setThumbnail(String thumbnail) {
 		this.thumbnail = thumbnail;
 	}
-	
-	
+
+
 	public boolean isSelf() {
 		return is_self;
 	}
@@ -55,6 +58,49 @@ public class Data2 {
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel out, int flags) {
+		out.writeString(title);
+		out.writeInt(score);
+		out.writeInt(num_comments);
+		out.writeString(thumbnail);
+		
+		out.writeByte((byte) (is_self ? 1 : 0)); 
+		out.writeString(selftext);
+		out.writeString(url);
+	}
+	
+	public static final Parcelable.Creator<Data2> CREATOR = new Parcelable.Creator<Data2>() {
+		public Data2 createFromParcel(Parcel in) {
+			return new Data2(in);
+		}
+
+		public Data2[] newArray(int size) {
+			return new Data2[size];
+		}
+	};
+
+	/**
+	 * Parcelable Constructor
+	 * @param in
+	 */
+	private Data2(Parcel in) {
+		title = in.readString();
+		score = in.readInt();
+		num_comments = in.readInt();
+		thumbnail = in.readString();
+		
+		is_self = in.readByte() != 0;
+		selftext = in.readString();
+		url = in.readString(); 
+		
+	}
+
 
 }
